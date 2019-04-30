80b65e6ce5b02256f0f617818e168d3d686738b1
spoonerf <spoonerfiona@gmail.com>
2019-04-30 10:29:47 +0100

    Code was failing when there was only one cell with forest. Resolved this by converting test_pts to matrix. Added in extra code to do a 10deg buffer and cap the buffer extents to the extent of the hansen dataset.

--- hd_1.R
+++ hd_1.R
@@ -14,6 +14,7 @@ hansen_dist_fun<-function(x){
   x<-data.frame(x[1], x[2])
   colnames(x)<-c("lon", "lat")
   extract_out<-raster::extract(hansen, x)
+  extract_out[is.na(extract_out) | extract_out == 255] <- 0
   dist_out<-NULL
   
   if(extract_out == 1){
@@ -30,12 +31,14 @@ hansen_dist_fun<-function(x){
     #subset to the cropped area
     test_pts <- rasterToPoints(test, function(x){!is.na(x)})
     test_pts<-test_pts[test_pts[,3] ==1,]
+  
+    test_pts <- matrix(test_pts, ncol = 3)
     
-    if(nrow(test_pts== 1)){
-      dist_out<-min(pointDistance(x, test_pts[1:2], lonlat = TRUE)/1000)
+    if(nrow(test_pts)==0){
+      dist_out<-NULL
     }
     
-    if(nrow(test_pts> 1)){
+    if(nrow(test_pts>= 1)){
       dist_out<-min(pointDistance(x, test_pts[,1:2], lonlat = TRUE)/1000)
     }
   }
@@ -51,11 +54,11 @@ hansen_dist_fun<-function(x){
     test_pts <- rasterToPoints(test, function(x){!is.na(x)})
     test_pts<-test_pts[test_pts[,3] ==1,]
     
-    if(nrow(test_pts== 1)){
-      dist_out<-min(pointDistance(x, test_pts[1:2], lonlat = TRUE)/1000)
+    if(nrow(test_pts)==0){
+      dist_out<-NULL
     }
     
-    if(nrow(test_pts> 1)){
+    if(nrow(test_pts>= 1)){
       dist_out<-min(pointDistance(x, test_pts[,1:2], lonlat = TRUE)/1000)
     }
     
@@ -72,11 +75,11 @@ hansen_dist_fun<-function(x){
     test_pts <- rasterToPoints(test, function(x){!is.na(x)})
     test_pts<-test_pts[test_pts[,3] ==1,]
     
-    if(nrow(test_pts== 1)){
-      dist_out<-min(pointDistance(x, test_pts[1:2], lonlat = TRUE)/1000)
+    if(nrow(test_pts)==0){
+      dist_out<-NULL
     }
     
-    if(nrow(test_pts> 1)){
+    if(nrow(test_pts>= 1)){
       dist_out<-min(pointDistance(x, test_pts[,1:2], lonlat = TRUE)/1000)
     }
     }
@@ -92,17 +95,42 @@ hansen_dist_fun<-function(x){
     test_pts <- rasterToPoints(test, function(x){!is.na(x)})
     test_pts<-test_pts[test_pts[,3] ==1,]
     
-    if(nrow(test_pts== 1)){
-      dist_out<-min(pointDistance(x, test_pts[1:2], lonlat = TRUE)/1000)
+    if(nrow(test_pts)==0){
+      dist_out<-NULL
     }
     
-    if(nrow(test_pts> 1)){
+    if(nrow(test_pts>= 1)){
       dist_out<-min(pointDistance(x, test_pts[,1:2], lonlat = TRUE)/1000)
     }
-    }
+  }
   
   if(is.null(dist_out)){
-    dist_out<-paste0("Further than 4 degrees from nearest forest!")  
+    #have capped the maximum cropping distance to 4 degrees so that we don't end up cropping beyond the extent of the original raster e.g. values >180 degrees
+    xmin<-x$lon -10
+    xmax<-x$lon +10
+    ymin<-x$lat -10
+    ymax<-x$lat +10
+    #making sure the extents of the crop don't go over the edge of the hansen data
+    xmin[xmin < -180]<- -180
+    xmax[xmax > 180]<- 180
+    ymin[xmin < -60]<- -60
+    ymax[xmin < 80]<- 80
+    test<-crop(hansen, c(xmin,xmax,ymin,ymax))
+    #subset to the cropped area
+    test_pts <- rasterToPoints(test, function(x){!is.na(x)})
+    test_pts<-test_pts[test_pts[,3] ==1,]
+    
+    if(nrow(test_pts)==0){
+      dist_out<-NULL
+    }
+    
+    if(nrow(test_pts>= 1)){
+      dist_out<-min(pointDistance(x, test_pts[,1:2], lonlat = TRUE)/1000)
+    }
+  }
+  
+  if(is.null(dist_out)){
+    dist_out<-paste0("Further than 10 degrees from nearest forest!")  
   }
   
   dist_out_df<-cbind(x, dist_out)
@@ -114,7 +142,7 @@ hansen_dist_fun<-function(x){
 
 pred_u<-as.matrix(unique(pred))
 
-fun_out<-apply(pred_u[1511:nrow(pred_u),], MARGIN = 1 ,FUN = hansen_dist_fun)
+fun_out<-apply(pred_u[3680:nrow(pred_u),], MARGIN = 1 ,FUN = hansen_dist_fun)
 all_dist<-do.call("rbind", fun_out)
 
 pred<-read.csv(paste0(here(),"/Fiona_help_Hansen_dataset/PREDICTS_NatPlusCrop_forestBiome_site_level_new.csv"))
